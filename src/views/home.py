345358f5f375061940a31b729e23002006a9fa1e
Aditya Tiwari <67660542+AdiTiwa@users.noreply.github.com>
2023-03-04 16:54:35 -0600

    idk

--- src/views/home.py
+++ src/views/home.py
@@ -2,6 +2,7 @@ import flet as ft
 
 
 def home(page: ft.Page):
+<<<<<<< Updated upstream
     return ft.View(
         "/",
         [
@@ -17,3 +18,31 @@ def home(page: ft.Page):
             ])
         ]
     )
+=======
+    if page.client_storage.contains_key("user_id"):
+        return ft.View(
+            "/",
+            [
+                ft.ElevatedButton('Login', on_click= lambda _: page.go('/logout')),
+                ft.Row([
+                    ft.Column([
+                        ft.Text("Stats", size=20),
+                        ft.Text("Strength: "),
+                        ft.Text("Endurance: "),
+                    ]),
+                    ft.Text("Avocado Goes Here", size = 50),
+                ])
+            ]
+        )
+    else:
+        return ft.View(
+            "/",
+            [
+                ft.Text("Join or Create an Account"),
+                ft.Row([
+                    ft.ElevatedButton('Login', on_click= lambda _: page.go('/login')),
+                    ft.ElevatedButton('Create an Account', on_click= lambda _: page.go('/create_account')),
+                ])
+            ]
+        )
+>>>>>>> Stashed changes
