c51f3707bf915977a7e07dabde2c5dcfa6d63973
shasahasr <shasahasr@gmail.com>
2023-03-04 15:16:45 -0600

    basic login page layout

--- src/views/login.py
+++ src/views/login.py
@@ -7,8 +7,20 @@ def login(page: ft.Page):
     return ft.View(
         '/login',
         [
-            ft.AppBar(title=ft.Text('Flet Login'), bgcolor=ft.colors.SURFACE_VARIANT),
-            ft.ElevatedButton('Go to homepage', on_click=lambda _: page.go('/')),
-            ft.ElevatedButton('hi hello', on_click=lambda _: upchange())
+            ft.Container(
+                ft.TextField(label="Username"),
+                alignment=ft.alignment.center,
+                padding=50,
+            ),
+            ft.Container(
+                ft.TextField(label="Password", password = True, can_reveal_password=True),
+                alignment=ft.alignment.center,
+                padding=50,
+            ),
+            ft.Container(
+                ft.FilledButton(text="Login"),
+                alignment=ft.alignment.center,
+                padding=50,
+            ),
         ]
     )
