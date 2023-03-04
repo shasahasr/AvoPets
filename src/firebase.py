b107c821ee08f67efb44bb2dd5636b865b4136c2
Aditya Tiwari <67660542+AdiTiwa@users.noreply.github.com>
2023-03-04 16:35:32 -0600

    registering!

--- src/firebase.py
+++ src/firebase.py
@@ -18,3 +18,11 @@ def check_login(email, password):
         if email == user.get("email") and password == user.get("password"):
             return user.id
     return False
+
+def register(email, password):
+    for user in users_ref.get():
+            if email == user.get('email'):
+                return False
+    
+    new_user = users_ref.add({"email": email, "password": password, "pet": {"endurance": 100, "health": 100, "name": "pet name", "strength": 100}, "stats": {"mindfulness": 0, "running": 0, "weights": 0}})
+    return new_user[1].id
--- src/views/login.py
+++ src/views/login.py
@@ -1,7 +1,7 @@
 import flet as ft
+
 import sys
 import os
-
 current = os.path.dirname(os.path.realpath(__file__))
 parent = os.path.dirname(current)
 sys.path.append(parent)
