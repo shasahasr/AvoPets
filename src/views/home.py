281195e58447dccd58d5d5762ea969b56a8b9474
Chanadu <chandu.peddada@gmail.com>
2023-03-04 16:59:05 -0600

    Commit Go wee woo

--- src/views/home.py
+++ src/views/home.py
@@ -5,15 +5,18 @@ def home(page: ft.Page):
     return ft.View(
         "/",
         [
-            ft.ElevatedButton('Login', on_click= lambda _: page.go('/login')),
-            ft.ElevatedButton('Create an Account', on_click= lambda _: page.go('/create_account')),
+            ft.ElevatedButton('Login', on_click=lambda _: page.go('/login')),
+            ft.ElevatedButton('Create an Account',
+                              on_click=lambda _: page.go('/create_account')),
+            ft.ElevatedButton('Train',
+                              on_click=lambda _: page.go('/train')),
             ft.Row([
                 ft.Column([
                     ft.Text("Stats", size=20),
                     ft.Text("Strength: "),
                     ft.Text("Endurance: "),
                 ]),
-                ft.Text("Avacado Goes Here", size = 50),
+                ft.Text("Avacado Goes Here", size=50),
             ])
         ]
     )
