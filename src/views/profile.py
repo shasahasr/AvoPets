183fcf7889f5c9334d6465948aa0a7e7f3de9a8d
Chanadu <chandu.peddada@gmail.com>
2023-03-05 02:39:05 -0600

    testings

--- src/views/profile.py
+++ src/views/profile.py
@@ -13,22 +13,7 @@ def profile(page: ft.Page):
                 alignment=ft.alignment.center,
             ),
             ft.Column(
-                controls=[ft.Container(
-                    ft.Text("Your Lifetime Stats", size=30),
-                    alignment=ft.alignment.center,
-                ),
-                    ft.Container(
-                    ft.Text("Mindfulness: ", size=20),
-                    alignment=ft.alignment.center,
-                ),
-                    ft.Container(
-                    ft.Text("Running: ", size=20),
-                    alignment=ft.alignment.center,
-                ),
-                    ft.Container(
-                    ft.Text("Weights: ", size=20),
-                    alignment=ft.alignment.center,
-                ),]
+                
             )
         ]
     )
