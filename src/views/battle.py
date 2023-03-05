3ff0220c57684882eb42d6374e72fd519184b892
shasahasr <shasahasr@gmail.com>
2023-03-04 19:22:12 -0600

    battle

--- src/views/battle.py
+++ src/views/battle.py
@@ -1,6 +1,7 @@
 import flet as ft
 
 def battle(page: ft.Page):
+    '/battle',
     return ft.View([
         ft.AppBar(title=ft.Text('Battle'), bgcolor=ft.colors.SURFACE_VARIANT),
         ft.Text("battle lmfao")
