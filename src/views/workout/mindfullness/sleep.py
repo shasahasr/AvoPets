1ff36e475a5030e91953ac33445ee8184203c3d6
Chanadu <chandu.peddada@gmail.com>
2023-03-04 21:33:11 -0600

    Assets cuz shaunak is hot garbage as usual like come on bro it is not that hard it is literally command c and command v from the dev article like bro how are you this bad

--- src/views/workout/mindfullness/sleep.py
+++ src/views/workout/mindfullness/sleep.py
@@ -7,5 +7,11 @@ def sleep(page: ft.Page):
         [
             ft.AppBar(title=ft.Text('Sleep'),
                       bgcolor=ft.colors.SURFACE_VARIANT),
+            ft.Image(
+                src=f"./assets/idle0.png",
+                width=100,
+                height=100,
+                fit=ft.ImageFit.CONTAIN,
+            )
         ]
     )
