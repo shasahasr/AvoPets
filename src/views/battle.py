ae30933d1382a2fdafc3919ce41462e7c16d7cf4
Aditya Tiwari <67660542+AdiTiwa@users.noreply.github.com>
2023-03-04 19:23:15 -0600

    battle again what the hell is happening

--- src/views/battle.py
+++ src/views/battle.py
@@ -1,8 +1,10 @@
 import flet as ft
 
 def battle(page: ft.Page):
-    '/battle',
-    return ft.View([
-        ft.AppBar(title=ft.Text('Battle'), bgcolor=ft.colors.SURFACE_VARIANT),
-        ft.Text("battle lmfao")
-    ])
+    return ft.View(
+        "/battle",
+        [
+            ft.AppBar(title=ft.Text('Battle'), bgcolor=ft.colors.SURFACE_VARIANT),
+            ft.Text("battle lmfao")
+        ]
+    )
