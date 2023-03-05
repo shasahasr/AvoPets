36338920a1a3ef84c7eaa942ef7c8d8d058fc632
Chanadu <chandu.peddada@gmail.com>
2023-03-04 18:41:45 -0600

    Cchhaannggeedd rofile home butotitn kon with back button and app taskbar

--- src/views/profile.py
+++ src/views/profile.py
@@ -1,29 +1,31 @@
 import flet as ft
 
+
 def profile(page: ft.Page):
     return ft.View(
         '/profile',
         [
-            ft.ElevatedButton(
-                'Home', on_click=lambda _: page.go('/home')),
+            ft.AppBar(title=ft.Text('Profile'),
+                      bgcolor=ft.colors.SURFACE_VARIANT),
             ft.Container(
-                ft.CircleAvatar(content=ft.Icon(ft.icons.ACCOUNT_CIRCLE_ROUNDED, size=180), radius=100),
-                alignment = ft.alignment.center,
+                ft.CircleAvatar(content=ft.Icon(
+                    ft.icons.ACCOUNT_CIRCLE_ROUNDED, size=180), radius=100),
+                alignment=ft.alignment.center,
             ),
-            ft.Column( 
-                controls = [ft.Container(
+            ft.Column(
+                controls=[ft.Container(
                     ft.Text("Your Lifetime Stats", size=30),
                     alignment=ft.alignment.center,
                 ),
-                ft.Container(
+                    ft.Container(
                     ft.Text("Mindfulness: ", size=20),
                     alignment=ft.alignment.center,
                 ),
-                ft.Container(
+                    ft.Container(
                     ft.Text("Running: ", size=20),
                     alignment=ft.alignment.center,
                 ),
-                ft.Container(
+                    ft.Container(
                     ft.Text("Weights: ", size=20),
                     alignment=ft.alignment.center,
                 ),]
