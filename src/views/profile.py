1f51a140a7744b76f46393fcd2a077cdcff684a7
shasahasr <shasahasr@gmail.com>
2023-03-04 18:38:17 -0600

    actually made a decent profile page

--- src/views/profile.py
+++ src/views/profile.py
@@ -6,6 +6,27 @@ def profile(page: ft.Page):
         [
             ft.ElevatedButton(
                 'Home', on_click=lambda _: page.go('/home')),
-            ft.CircleAvatar(content=ft.Icon(ft.icons.ACCOUNT_CIRCLE_ROUNDED),),
+            ft.Container(
+                ft.CircleAvatar(content=ft.Icon(ft.icons.ACCOUNT_CIRCLE_ROUNDED, size=180), radius=100),
+                alignment = ft.alignment.center,
+            ),
+            ft.Column( 
+                controls = [ft.Container(
+                    ft.Text("Your Lifetime Stats", size=30),
+                    alignment=ft.alignment.center,
+                ),
+                ft.Container(
+                    ft.Text("Mindfulness: ", size=20),
+                    alignment=ft.alignment.center,
+                ),
+                ft.Container(
+                    ft.Text("Running: ", size=20),
+                    alignment=ft.alignment.center,
+                ),
+                ft.Container(
+                    ft.Text("Weights: ", size=20),
+                    alignment=ft.alignment.center,
+                ),]
+            )
         ]
     )
