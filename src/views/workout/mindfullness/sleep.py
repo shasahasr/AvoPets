987dfca23aa1d2639fe045a959337a6ad25b4d1c
Chanadu <chandu.peddada@gmail.com>
2023-03-05 01:35:32 -0600

    Sleep working

--- src/views/workout/mindfullness/sleep.py
+++ src/views/workout/mindfullness/sleep.py
@@ -5,10 +5,12 @@ def sleep(page: ft.Page):
     return ft.View(
         '/sleep',
         [
+
             ft.AppBar(title=ft.Text('Sleep'),
                       bgcolor=ft.colors.SURFACE_VARIANT),
             ft.Text("Sleep Good pls"),
             ft.Text("How many hours did you sleep last night?"),
-            ft.Slider(min=0, max=15, divisions=1, value=8, label="{value}")
+            ft.Slider(min=0, max=15, divisions=15,
+                      value=8, label="{value} Hours")
         ]
     )
